25c23812421f1eae08906db4acb7ef7343767e2f
agmas <Maxlion360@gmail.com>
2024-07-20 01:59:43 +0400

    :3333

--- export/html5/bin/PirateJam2024.js
+++ export/html5/bin/PirateJam2024.js
@@ -900,7 +900,7 @@ ApplicationMain.main = function() {
 ApplicationMain.create = function(config) {
 	var app = new openfl_display_Application();
 	ManifestResources.init(config);
-	app.meta.h["build"] = "117";
+	app.meta.h["build"] = "4";
 	app.meta.h["company"] = "HaxeFlixel";
 	app.meta.h["file"] = "PirateJam2024";
 	app.meta.h["name"] = "PirateJam2024";
@@ -70179,7 +70179,7 @@ var lime_utils_AssetCache = function() {
 	this.audio = new haxe_ds_StringMap();
 	this.font = new haxe_ds_StringMap();
 	this.image = new haxe_ds_StringMap();
-	this.version = 115652;
+	this.version = 880418;
 };
 $hxClasses["lime.utils.AssetCache"] = lime_utils_AssetCache;
 lime_utils_AssetCache.__name__ = "lime.utils.AssetCache";
