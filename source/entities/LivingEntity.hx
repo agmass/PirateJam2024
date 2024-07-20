74fb33d10b6dd3cee67d3c509a4ed6168b74c3dd
agmas <Maxlion360@gmail.com>
2024-07-20 17:10:36 +0400

    im so done with workflows anyways enemiesss

--- source/entities/LivingEntity.hx
+++ source/entities/LivingEntity.hx
@@ -1,18 +1,36 @@
 package entities;
 
+import flixel.FlxG;
 import flixel.FlxSprite;
+import util.Hitbox;
 
 class LivingEntity extends FlxSprite {
 
-    public var health = 0.0;
+	public var health = 100.0;
     public var movementSpeed = 500;
     public var movementModifications:Map<String, Int> = new Map();
     
-    public var invincible = false;
+	public var invincible = false;
+	public var destroyOnDeath = true;
 
 
     public function death() {
         alive = false;
+		if (destroyOnDeath)
+		{
+			FlxG.state.remove(this);
+			destroy();
+		}
+	}
+
+	public function getMovementSpeed():Int
+	{
+		var finalSpeed = movementSpeed;
+		for (i in movementModifications)
+		{
+			finalSpeed += i;
+		}
+		return finalSpeed;
     }
 
     public function damage(damage:Float):Bool {
@@ -20,6 +38,7 @@ class LivingEntity extends FlxSprite {
             return false;
         } else {
             health -= damage;
+
             if (health <= 0) {
                 death();
             }
